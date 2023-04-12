34400748a66fbee43ff4dc9e6e249e3662841991
912-Cotor-Catinca <catincaflorina@gmail.com>
2023-04-12 09:19:00 +0300

    push changes

--- src/test/java/org/example/BigBangTest.java
+++ src/test/java/org/example/BigBangTest.java
@@ -13,7 +13,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class BigBang {
+public class BigBangTest {
     @Test
     public void shouldAnswerWithTrue()
     {
